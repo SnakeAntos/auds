4eef76422c68095efe0870d21fcab8fa009ce88e
josefinaarbilla <josefinaarbillap@gmail.com>
2023-07-19 19:17:40 -0300

    modificacion common components

--- audn-app/src/components/Common/Input/input.jsx
+++ audn-app/src/components/Common/Input/input.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export const Input = (props) => {
   return (
     <div>
-      <input className="input">{props.text}</input>
+      <input className="input" type={props.type}>{props.text}</input>
     </div>
   );
 };
