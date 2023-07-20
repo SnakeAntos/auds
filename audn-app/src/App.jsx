2f986a71af848b8a82fa6d928f87ca75c4a834ff
Joaquin Elizondo <juacaelizondo@gmail.com>
2023-07-20 19:12:50 -0300

    trabajo comp musicaya

--- audn-app/src/App.jsx
+++ audn-app/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
+import { Home1 } from "./components/Home1/home1";
 //import { LandingPage } from "./components/LandingPage/landingPage";
-import { LogIn } from "./components/LogIn/logIn";
+//import { LogIn } from "./components/LogIn/logIn";
 //import { Register } from "./components/Register/register";
 
 export const App = () => {
   return (
     <>
-      <LogIn />
+      <Home1 />
     </>
   );
 };
---
+++ audn-app/src/components/Common/Button/buttonMusicaYa.jsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export const ButtonMusicaYa = (props) => {
+  return <button className="button button-transparent">{props.text}</button>;
+};
