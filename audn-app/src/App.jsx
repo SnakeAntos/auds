040fd44d621b53c2ca55c7f116a62569ea06947f
Joaquin Elizondo <juacaelizondo@gmail.com>
2023-07-19 21:35:57 -0300

    componente login

--- audn-app/src/App.jsx
+++ audn-app/src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import { LandingPage } from "./components/LandingPage/landingPage";
+//import { LandingPage } from "./components/LandingPage/landingPage";
 import { LogIn } from "./components/LogIn/logIn";
-import { Register } from "./components/Register/register";
+//import { Register } from "./components/Register/register";
 
 export const App = () => {
   return (
     <>
-      <LandingPage />
-      <Register />
       <LogIn />
     </>
   );
