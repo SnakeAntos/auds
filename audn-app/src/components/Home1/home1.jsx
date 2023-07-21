af2a6ddd89b5813bc03e09326015457072022f13
Joaquin Elizondo <juacaelizondo@gmail.com>
2023-07-20 21:15:30 -0300

    comp musicaYa

--- audn-app/src/components/Home1/home1.jsx
+++ audn-app/src/components/Home1/home1.jsx
@@ -20,8 +20,40 @@ export const Home1 = (props) => {
         </div>
       </div>
       <div className="musicaya-main">
-        <ButtonMusicaYa className="button-transparent"></ButtonMusicaYa>
-        <ButtonMusicaYa className="button-transparent"></ButtonMusicaYa>
+        <div className="musicaya-btn1">
+          <div className="button-musicaYa1">
+            <img
+              className="photo-my1"
+              src="../../../public/images/musicaYa1.png"
+            ></img>
+          </div>
+          <div className="cupido-text">
+            <h3 className="title-cupido">Cupido Musical</h3>
+            <p className="text-cupido">
+              Tus artistas favoritos nunca van a dejarte con el corazon roto.
+            </p>
+          </div>
+          <div>
+            <ButtonMusicaYa className="button-transparent"></ButtonMusicaYa>
+          </div>
+        </div>
+        <div className="musicaya-btn1">
+          <div className="button-musicaYa1">
+            <img
+              className="photo-my1"
+              src="../../../public/images/musicaYa2.png"
+            ></img>
+          </div>
+          <div className="cupido-text">
+            <h3 className="title-cupido">Musica Contextual</h3>
+            <p className="text-cupido">
+              Creamos la playlist perfecta para cualquier situacion.
+            </p>
+          </div>
+          <div>
+            <ButtonMusicaYa className="button-transparent"></ButtonMusicaYa>
+          </div>
+        </div>
       </div>
     </div>
   );
