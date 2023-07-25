3a8ff7fce6dbb0c10be5eb2c2779cb61d17be429
SnakeAntos <snake.fede@gmail.com>
2023-07-25 19:12:47 -0300

    Fetch generico exitoso

--- audn-app/src/App.jsx
+++ audn-app/src/App.jsx
@@ -26,7 +26,7 @@ export const App = () => {
    // Función asincrónica para obtener todos los usuarios
    const fetchUsers = async () => {
     try {
-      const response = await fetch("http://localhost:3000/users/allusers");
+      const response = await fetch("http://localhost:3001/users/allusers");
       const data = await response.json();
       setUsers(data);
       console.log(users);
