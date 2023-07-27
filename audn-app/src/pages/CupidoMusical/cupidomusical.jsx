22f377dab29f2c23abbf85d2f95d475a570d37dd
Joaquin Elizondo <juacaelizondo@gmail.com>
2023-07-26 22:34:34 -0300

    comp musicacontext

--- audn-app/src/pages/CupidoMusical/cupidomusical.jsx
+++
@@ -1,8 +0,0 @@
-import './styles.css'
-
-export const CupidoMusical = () => {
-    return(
-        <>
-        </>
-    )
-}
--- audn-app/src/pages/MusicaContextual/musicaContextual.jsx
+++ audn-app/src/pages/MusicaContextual/musicaContextual.jsx
@@ -5,6 +5,9 @@ import { ButtonGrey } from "../../components/Common/Button/buttonGrey";
 import { ButtonsMusicaYa } from "../../components/Common/Button/buttonsMusicaYa";
 
 export const MusicaContextual = (props) => {
+  const menuOptions = ["Ejercicio Fisico", "Limpieza", "Celebracion"];
+  const menuOptions1 = ["Muy Contento", "Triste", "Con todo el Power"];
+  const menuOptions2 = ["Frio", "Soleado", "de playa"];
   const buttonTexts = [
     "Rock",
     "Country",
@@ -16,6 +19,14 @@ export const MusicaContextual = (props) => {
     "Reggae",
     "Folk",
     "R&B",
+    "Clasico",
+    "Alterntivo",
+    "Ambiente",
+    "EDM",
+    "Electronica",
+    "Disco",
+    "New Age",
+    "Punk",
   ];
   return (
     <div className="musica-cont-nav">
@@ -31,24 +42,44 @@ export const MusicaContextual = (props) => {
       <div className="musica-cont-body">
         <div>
           <h3 className="musicacont-title1">Cual es la ocasion?</h3>
-          <ButtonMusicaContextual placeholder="Actividad" />
+          <ButtonMusicaContextual
+            text="Actividad"
+            textColor="grey"
+            options={menuOptions}
+          />
         </div>
         <div>
           <h3 className="musicacont-title1">Como te sientes?</h3>
-          <ButtonMusicaContextual placeholder="Actividad" />
+          <ButtonMusicaContextual
+            text="Estado de Animo"
+            textColor="grey"
+            options={menuOptions1}
+          />
         </div>
         <div>
-          <h3 className="musicacont-title1">Como estas el clima?</h3>
-          <ButtonMusicaContextual placeholder="Actividad" />
+          <h3 className="musicacont-title1">Como esta el clima?</h3>
+          <ButtonMusicaContextual
+            text="Clima"
+            textColor="grey"
+            options={menuOptions2}
+          />
         </div>
       </div>
-      <h3 className="musicacont-title1">Selecciona hasta 3 generos:</h3>
-      {buttonTexts.map((text, index) => (
-        <div key={index} className="musica-cont-buttons-fit">
-          <ButtonsMusicaYa text={text} />
+      <div className="box">
+        <h3 className="musicacont-title1">Selecciona hasta 3 generos:</h3>
+        <div className="musica-contextual-container">
+          <div className="musica-contextual-box">
+            {buttonTexts.map((text, index) => (
+              <div key={index} className="musica-cont-buttons-fit">
+                <ButtonsMusicaYa text={text} />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-      <ButtonGrey text="Crear Playlist" />
+      </div>
+      <div className="crear-playlist-button">
+        <ButtonGrey text="Crear Playlist" />
+      </div>
     </div>
   );
 };
